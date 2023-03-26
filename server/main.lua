84040803bd670eaafc85b93b81412b2aced53817
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-03-26 19:18:34 +0200

    fix (use correct event)

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-AddEventHandler('hospital:server:SetDeathStatus', function(isDead)
+RegisterNetEvent('hospital:server:SetLaststandStatus', function(isDead)
     TriggerClientEvent('radialmenu:client:deadradial', source, isDead)
 end)
