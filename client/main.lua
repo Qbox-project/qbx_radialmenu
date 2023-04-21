6b7bcac668c2d1b55c47d99a1cc0e70b3ea69408
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-04-21 19:44:22 +0200

    hmm yeah

--- client/main.lua
+++ client/main.lua
@@ -22,11 +22,7 @@ local function convert(tbl)
             action = function() TriggerServerEvent(tbl.event, tbl.arg or nil) end
         end
     elseif tbl.action then
-        action = function()
-            if type(_G[tbl.action]) == 'function' then
-                _G[tbl.action](tbl.arg or nil)
-            end
-        end
+        action = tbl.action
     elseif tbl.command then
         if tbl.type == 'command' then
             action = function() ExecuteCommand(tbl.event) end
