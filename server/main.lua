8b72698a4c85d824f6aab6154563f816aa9f6a43
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-05-06 02:17:52 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,3 +3,8 @@ QBCore = exports['qbx-core']:GetCoreObject()
 RegisterNetEvent('hospital:server:SetLaststandStatus', function(isDead)
     TriggerClientEvent('radialmenu:client:deadradial', source, isDead)
 end)
+
+-- qb-amublancejob compatibility
+RegisterNetEvent('hospital:server:SetDeathStatus', function(isDead)
+    TriggerClientEvent('radialmenu:client:deadradial', source, isDead)
+end)
