e0228556fbe20da80b9d1f42e3cd2ee95d6e39a8
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-02 21:21:09 -0500

    feat: remove stretcher

--- config.lua
+++ config.lua
@@ -525,28 +525,7 @@ Config.JobInteractions = {
             type = 'client',
             event = 'police:client:EscortPlayer',
             shouldClose = true
-        }, {
-            id = 'stretcheroptions',
-            title = 'Stretcher',
-            icon = 'bed-pulse',
-            items = {
-                {
-                    id = 'spawnstretcher',
-                    title = 'Spawn Stretcher',
-                    icon = 'plus',
-                    type = 'client',
-                    event = 'qb-radialmenu:client:TakeStretcher',
-                    shouldClose = false
-                }, {
-                    id = 'despawnstretcher',
-                    title = 'Remove Stretcher',
-                    icon = 'minus',
-                    type = 'client',
-                    event = 'qb-radialmenu:client:RemoveStretcher',
-                    shouldClose = false
-                }
-            }
-        }
+        },
     },
     ["taxi"] = {
         {
