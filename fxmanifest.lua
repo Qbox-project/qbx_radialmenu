fcba8db25c2d9bc77460dffaf146c0a1760c3eee
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2024-02-09 19:50:09 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,14 @@ game 'gta5'
 description 'qbx_radialmenu'
 repository 'https://github.com/Qbox-project/qbx_radialmenu'
 version '0.1.0'
+ox_lib 'locale'
+
 
 shared_scripts {
-    '@ox_lib/init.lua',
+     '@ox_lib/init.lua',
     '@qbx_core/modules/lib.lua',
 }
 
-ox_lib 'locale'
 
 client_scripts {
     '@qbx_core/modules/playerdata.lua',
