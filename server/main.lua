caf9bc3d059ba77d41c0197f6597aa9819c304ea
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-03-04 15:26:09 +0100

    fix radialmenu on death

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,6 @@
-RegisterNetEvent('hospital:server:SetLaststandStatus', function(isDead)
-    TriggerClientEvent('radialmenu:client:deadradial', source, isDead)
+AddStateBagChangeHandler('qbx_medical:deathState', nil, function(bagName, _, value)
+    local playerId = GetPlayerFromStateBagName(bagName)
+    TriggerClientEvent('radialmenu:client:deadradial', playerId, value == 2 or value == 3)
 end)
 
 -- qb-amublancejob compatibility
